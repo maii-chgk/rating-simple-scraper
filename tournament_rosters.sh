471cf107273c726ad7a367988039b1e92fcf500a
Jury Razumau <jury.razumau@zendesk.com>
2021-10-24 13:24:23 +0200

    source rvm

--- tournament_rosters.sh
+++ tournament_rosters.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-/usr/share/rvm/bin/rvm ruby-3.0.2 do bundle exec ruby /home/deploy/rating-simple-scraper/tournament_rosters.rb > tournament_rosters.log
+source "/usr/share/rvm/bin/rvm"
+cd /home/deploy/rating-simple-scraper/
+rvm use 3.0.2
+bundle exec ruby tournament_rosters.rb
