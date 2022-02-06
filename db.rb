86c04ea2fb2ce958642c99a1546bbb6d3f4d4ed1
Jury Razumau <jurazumau@gmail.com>
2022-02-06 14:21:13 +0100

    clean up db connection

--- db.rb
+++ db.rb
@@ -2,4 +2,3 @@ require 'sequel'
 
 connection_string = ENV.fetch('CONNECTION_STRING', 'postgres://localhost/postgres')
 DB = Sequel.connect(connection_string)
-# DB = Sequel.connect('postgres://simple_scraper_cron:pv44H78XmNnuVwsfaNWu9gnKTyDy3Uq2DisABhQN4@104.248.17.45:5432/public')
